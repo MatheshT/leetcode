120e16e7b800068b431163ec2913531ad4783e70
MATHESH T <matheshrmd@gmail.com>
2026-08-13 09:36:58 +0530

    Add toLowerCase function in 709. To Lower Case
    Implement a function to convert a string to lowercase.

---
+++ 709. To Lower Case.py
@@ -0,0 +1,24 @@
+'''709. To Lower Case
+
+
+
+
+
+
+
+Example 1:
+
+Input: s = "Hello"
+Output: "hello"'''
+
+
+#code link:https://leetcode.com/problems/to-lower-case/description/
+
+
+
+
+
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        c=s.lower()
+        return c
