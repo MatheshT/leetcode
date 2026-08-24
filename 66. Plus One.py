8b3428819882325e9fdc90a4b93b7bbf23fb146a
MATHESH T <matheshrmd@gmail.com>
2026-08-24 21:05:12 +0530

    Create 66. Plus One.py

---
+++ 66. Plus One.py
@@ -0,0 +1,27 @@
+'''66. Plus One
+
+
+Example 1:
+
+Input: digits = [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Incrementing by one gives 123 + 1 = 124.
+Thus, the result should be [1,2,4].'''
+
+
+
+#code link:https://leetcode.com/problems/plus-one/description/
+
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in range(len(digits) - 1, -1, -1):
+
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+
+            digits[i] = 0
+
+        return [1] + digits
